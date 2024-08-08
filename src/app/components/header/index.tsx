9a71cb83bde9a83ef49ce90ae522263d894a978a
b_e_sa <joaospereira963@gmail.com>
2024-08-08 17:38:04 -0300

    finish header responsiviness

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,11 +6,11 @@ import styles from "./header.module.scss";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <NoteIcon />
+      <NoteIcon className={styles["note-icon"]} />
       <p>CoreNotes</p>
       <div className={styles["search-bar"]}>
         <input type="text" placeholder="Pesquisar notas" />
-        <MagnifierIcon />
+        <MagnifierIcon className={styles["magnifier-icon"]}/>
       </div>
       <XIcon className={styles["x-icon"]} />
     </header>
