2748874bf0cc90e08dcee501f17bf7068ca64ef9
b_e_sa <joaospereira963@gmail.com>
2024-08-11 13:11:28 -0300

    implement get todos api call and treatment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,78 @@
+"use client";
+
 import { Inter } from "next/font/google";
-import NewTodo from "./components/todo/new-todo/new-todo";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import Todo from "./components/todo/editable-todo";
+import NewTodo from "./components/todo/new-todo/new-todo";
+import { api } from "./lib/axios";
 import styles from "./page.module.scss";
+import { useTodoStore } from "./stores/todo-store";
+import { useInView } from "react-intersection-observer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const favoritedTodos = Array(2).fill(5);
-  const otherTodos = Array(10).fill(5);
+  const {
+    increaseApiCallQuantity,
+    apiCallQuantity,
+    updateStoreTodos,
+    favoriteTodos,
+    otherTodos,
+  } = useTodoStore((state) => state);
+
+  const [inMidRequest, setInMidRequest] = useState(false);
+
+  const { ref: favoritesRef, inView: favoritesBottomInView } = useInView();
+  const { ref: othersRef, inView: othersBottomInView } = useInView();
+
+  useEffect(() => {
+    if (othersBottomInView || favoritesBottomInView) {
+      if (!inMidRequest) {
+        setInMidRequest(true);
+        api
+          .get("/todos", { params: { skip: apiCallQuantity * 10 } })
+          .then((res) => {
+            updateStoreTodos(res.data);
+            increaseApiCallQuantity();
+          })
+          .catch(() => {
+            toast(
+              "Algum erro ocorreu ao recuperar seus todos. Tente novamente"
+            );
+          })
+          .finally(() => setInMidRequest(false));
+      }
+    }
+  }, [othersBottomInView, favoritesBottomInView]);
 
   return (
     <main className={`${styles.main} ${inter.className}`}>
-      <NewTodo className={styles["new-todo"]}/>
-      <div className={styles.favorites}>
-        <p className={styles.label}>Favoritas</p>
-        <div className={styles["favorite-todos-container"]}>
-          {favoritedTodos.map((_, i) => {
-            return <Todo key={i} />;
-          })}
+      <NewTodo className={styles["new-todo"]} />
+      {favoriteTodos.length !== 0 && (
+        <div className={styles.favorites}>
+          <p className={styles.label}>Favoritas</p>
+          <div className={styles["favorite-todos-container"]}>
+            {favoriteTodos?.map((todo) => {
+              return <Todo key={todo.id} todoId={todo.id} {...(todo as any)} />;
+            })}
+            <div ref={favoritesRef} className={styles.loader}></div>
+          </div>
         </div>
-      </div>
+      )}
       <div className={styles.other}>
         <p className={styles.label}>Outras</p>
         <div className={styles["todos-container"]}>
-          {otherTodos.map((_, i) => {
-            return <Todo key={i} />;
+          {otherTodos?.map((todo) => {
+            return (
+              <Todo
+                key={todo.id}
+                todoId={todo.id}
+                {...(todo as any)}
+              />
+            );
           })}
+          <div ref={othersRef} className={styles.loader}></div>
         </div>
       </div>
     </main>
